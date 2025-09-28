a3454e586ea7f5e81e1de2576082001b52f4e284
bertholacosta <bertholpro@gmail.com>
2025-09-28 17:59:10 -0400

    solo primer ciclo

--- routes/empleadoRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const registroController = require("../controllers/empleadoController.js");  
-
-router.post("/", registroController.createEmpleado);
-
-router.get("/", registroController.mostrarEmpleados);
-
-module.exports = router;
