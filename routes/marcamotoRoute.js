18070e59476332c229879699c13946d15e4c58e9
bertholacosta <bertholpro@gmail.com>
2025-10-18 18:24:21 -0400

    correction

--- routes/marcamotoRoute.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const marcamotoController = require('../controllers/marcamotoController');
-
-router.post('/', marcamotoController.createmarcamoto);
-
-module.exports = router
